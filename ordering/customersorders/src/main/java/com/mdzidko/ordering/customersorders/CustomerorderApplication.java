f5778ca48400ee21e1b9f16716d9b7259e2b5872
mdzidko <mdzidko@novomatic-tech.com>
2020-05-31 19:45:58 +0200

    19- Service discovery with Consul

--- ordering/customersorders/src/main/java/com/mdzidko/ordering/customersorders/CustomerorderApplication.java
+++ ordering/customersorders/src/main/java/com/mdzidko/ordering/customersorders/CustomerorderApplication.java
@@ -2,13 +2,12 @@ package com.mdzidko.ordering.customersorders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerorderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerorderApplication.class, args);
 	}
-
 }
